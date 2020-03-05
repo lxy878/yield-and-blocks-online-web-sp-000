4279151d4019e36a99f6355d0667cecc00df25a4
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 11:56:04 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
-
+def hello_t(names)
+  if block_given?
+    i = 0
+    while i<names.length
+      yield names[i]
+      i+=1
+    end
+    names
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
 
+names = ["Tim", "Tom", "Jim"]
+hello_t(names) do |name|
+  puts "#{name}"
+end
+hello_t(names)
